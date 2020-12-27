4f3f83af78af030b5827f7dbacad07f492754ab8
Dio <srw109065@gmail.com>
2020-12-28 00:01:13 +0800

    '讀取紀錄並且繼續新增記帳'

--- products.py
+++ products.py
@@ -1,4 +1,15 @@
+#讀取檔案
 products = []
+with open('products.csv', 'r', encoding='utf-8') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue #跳到下一迴繼續
+		name, price  = line.strip().split(',')
+		products.append([name,price])
+print(products)
+
+
+#運行方式
 while True:
 	name = input("請輸入商品名稱: ")
 	if name == 'q':
@@ -13,19 +24,8 @@ while True:
 	#p = [name, price]
 	#更快速的方式
 	products.append([name, price])
-print(products)
-#把清單一個一個 拿出來打印
-for p in products:
-	print(p[0], '的價格是', p[1], '元')
-#寫入Text
+#寫入檔案
 with open('products.csv', 'w', encoding='utf-8') as f: #encoding 解決編碼無法顯示中文
 	f.write('商品,價格\n') #用來增加Excel 欄位名稱
 	for p in products:
 		f.write(p[0] + ',' + str(p[1]) + '\n')
-#讀取Text
-k = []
-with open('products1.txt', 'r') as f:
-	for p in f:
-		print(p.strip())
-		k.append(p.strip())
-print(k)
