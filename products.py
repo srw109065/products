8d90d6420eb8a49c41c592f5637ec8f065569428
Dio <srw109065@gmail.com>
2021-01-03 18:13:31 +0800

    '記帳最終版'

--- products.py
+++ products.py
@@ -1,13 +1,18 @@
-#讀取檔案
-products = []
-with open('products.csv', 'r', encoding='utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue #跳到下一迴繼續
-		name, price  = line.strip().split(',')
-		products.append([name,price])
-print(products)
+import os # operating system
 
+products = []
+#讀取檔案
+if os.path.isfile('products.csv'):#檢查檔案
+	print('找到檔愛了!')
+	with open('products.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			if '商品,價格,日期' in line:
+				continue #跳到下一迴繼續
+			name, price, date  = line.strip().split(',')
+			products.append([name,price,date])
+	print(products)
+else:
+	print('找不到檔案...')
 
 #運行方式
 while True:
@@ -16,6 +21,7 @@ while True:
 		break
 	price = input("請輸入商品價格: ")
 	price = int(price)
+	date = input("請輸入購買日期: ")
 	#原始方式
 	#p = []
 	#p.append(name)
@@ -23,9 +29,14 @@ while True:
 	#三種結合的簡潔方式
 	#p = [name, price]
 	#更快速的方式
-	products.append([name, price])
+	products.append([name, price,date])
+
+#印出所有購買紀錄
+for p in products:
+	print(p[0], '的價格是', p[1])
+
 #寫入檔案
 with open('products.csv', 'w', encoding='utf-8') as f: #encoding 解決編碼無法顯示中文
-	f.write('商品,價格\n') #用來增加Excel 欄位名稱
+	f.write('商品,價格,日期\n') #用來增加Excel 欄位名稱
 	for p in products:
-		f.write(p[0] + ',' + str(p[1]) + '\n')
+		f.write(p[0] + ',' + str(p[1]) + ',' + (p[2]) + '\n')
