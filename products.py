ce19f46f5af987e855832919ce56ce7819c7bec4
Dio <srw109065@gmail.com>
2022-01-18 20:57:39 +0800

    版本最新

--- products.py
+++ products.py
@@ -1,14 +1,30 @@
+
 products = []
+
+with open("products.csv", "r") as f:
+    for line in f:
+        if "商品,價格" in line:
+            continue
+        name, price = line.strip().split(",")
+        products.append([name,price])
+print(products)
+
 
 while True:
     name = input("請輸入商品名稱: ")
     if name == "q":
         break
     price = input("請輸入商品價格: ")
+    price = int(price)
     p = [name, price]
     products.append(p)
 
-print(products)
-
 for p in products:
 	print(p[0], "的價格是:", p[1])
+	
+pr = "商品,價格\n"
+
+with open("products.csv", "w") as f:
+	f.write(pr)
+	for p in products:
+		f.write(p[0] + "," + str(p[1]) + "\n")
