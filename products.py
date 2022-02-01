1350127f33cbdde31d035056c87a53736f222973
Dio <srw109065@gmail.com>
2022-02-01 23:06:25 +0800

    完善的購買本

--- products.py
+++ products.py
@@ -1,15 +1,21 @@
+import os 
 
 products = []
 
-with open("products.csv", "r") as f:
-    for line in f:
-        if "商品,價格" in line:
-            continue
-        name, price = line.strip().split(",")
-        products.append([name,price])
-print(products)
+if os.path.isfile("products.csv"):
+    print("找到了")
+    #讀取檔案    
+    with open("products.csv", "r") as f:
+        for line in f:
+            if "商品,價格" in line:
+                continue
+            name, price = line.strip().split(",")
+            products.append([name,price])
+    print(products)
+else:
+    print("沒有找到...")
 
-
+#使用者輸入
 while True:
     name = input("請輸入商品名稱: ")
     if name == "q":
@@ -18,12 +24,12 @@ while True:
     price = int(price)
     p = [name, price]
     products.append(p)
-
+#印出購買紀錄
 for p in products:
 	print(p[0], "的價格是:", p[1])
 	
 pr = "商品,價格\n"
-
+#寫入檔案
 with open("products.csv", "w") as f:
 	f.write(pr)
 	for p in products:
